be41e48cd27ac72cc02692067d0c4f5389bdb803
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-07-17 16:34:15 -0300

    Add files via upload
    Setando botão no SceneBuilder

---
+++ JavaFX2/src/gui/ViewController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+	
+	@FXML
+	private Button  btTeste;
+	
+	@FXML
+	public void onBtTesteAction() {
+		System.out.println("Clique!");
+		
+	}
+
+}
